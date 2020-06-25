9f373630fdd1cb128d611796e568ff691fc74317
Mauricio Rodriguez <mrodrigg@gmail.com>
2020-06-25 15:32:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NodeJS training blog project
+
+This repo contains a small blog NodeJS project from a Udemy course found at <https://www.udemy.com/course/advanced-node-for-developers/>.
---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:'',
+    googleClientSecret: '',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '124124124',
+    redisUrl: 'redis://127.0.0.1:6379',
+  };
